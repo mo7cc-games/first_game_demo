5516b9281dc053e18ce5e75605ec0262f85bf184
墨七 <meichangliang@outlook.com>
2025-04-20 05:16:07 +0800

    给游戏加料

--- hud/hud.gd
+++ hud/hud.gd
@@ -21,12 +21,11 @@ func show_game_over():
 	await get_tree().create_timer(1.0).timeout
 	$StartButton.show()
 
-
 func update_score(score):
 	$ScoreLabel.text = str(score)
 
-
 func _on_start_button_pressed():
+	print("Start button pressed")
 	$StartButton.hide()
 	start_game.emit()
 
